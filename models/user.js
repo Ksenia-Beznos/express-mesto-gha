03e0c9dc3e3d24e2a195acc891ece1c20db79336
Kseniia <ksenia.beznos@yandex.ru>
2023-05-20 00:53:55 +0300

    final_fix_1

--- models/user.js
+++ models/user.js
@@ -5,43 +5,45 @@ const userSchema = new mongoose.Schema(
   {
     email: {
       type: String,
-      required: [true, 'введите email'],
+      required: [true, "введите email"],
       unique: true,
       validate: {
         validator: (a) => validator.isEmail(a),
-        message: 'Неверный email',
+        message: "Неверный email",
       },
     },
 
     password: {
       type: String,
       select: false,
-      required: [true, 'введите пароль'],
+      required: [true, "введите пароль"],
     },
 
     name: {
       type: String,
-      default: 'Жак-Ив Кусто',
-      minlength: [2, 'минимальное кол-во символов - 2'],
-      maxlength: [30, 'максимальное кол-во символов - 30'],
+      required: true,
+      default: "Жак-Ив Кусто",
+      minlength: [2, "минимальное кол-во символов - 2"],
+      maxlength: [30, "максимальное кол-во символов - 30"],
     },
     about: {
       type: String,
-      default: 'Исследователь',
-      minlength: [2, 'минимальное кол-во символов - 2'],
-      maxlength: [30, 'максимальное кол-во символов - 30'],
+      required: true,
+      default: "Исследователь",
+      minlength: [2, "минимальное кол-во символов - 2"],
+      maxlength: [30, "максимальное кол-во символов - 30"],
     },
     avatar: {
       type: String,
       default:
-        'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+        "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
       validate: {
         validator: (a) => validator.isUrl(a),
-        message: 'Неверный email',
+        message: "Неверный email",
       },
     },
   },
-  { versionKey: false },
+  { versionKey: false }
 );
 
 module.exports = mongoose.model('User', userSchema);
